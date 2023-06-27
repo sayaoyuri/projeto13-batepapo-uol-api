d5c5b2ad27bccc9d5e9aefc7d2c23985a35db6cd
sayaoyuri <yuriantunes0000@gmail.com>
2023-06-27 16:57:09 -0300

    Add POST /participants endpoint - Storing data locally

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import express from 'express';
 import cors from 'cors';
+import Joi from 'joi';
+import dayjs from 'dayjs';
+
+import { messages, participants } from './mockData.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.post('/participants', (req, res) => {
+  const schema = Joi.object({ name: Joi.string().alphanum().required() });
+  const { name } = req.body;
+  
+  const { error } = schema.validate( { name } );
+  if(error) return res.sendStatus(422);
+  
+  if(participants.map(user => user.name).includes(name)) return res.sendStatus(409);
+
+  const time = Date.now();
+  
+  participants.push({ name, lastStatus: time });
+  messages.push(
+    { 
+      from: name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs(time).format("HH:mm:ss")
+    }
+  );
+
+  console.log(participants);
+  
+  res.sendStatus(201);
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running on http:/localhost:${PORT}/`));
---
+++ src/mockData.js
@@ -0,0 +1,16 @@
+export const participants = [
+  {
+		name: 'Joaozinho',
+		lastStatus: Date.now()
+  }
+];
+
+export const messages = [
+  { 
+		from: 'Joaozinho',
+		to: 'Todos',
+		text: 'entra na sala...',
+		type: 'status',
+		time: 'HH:mm:ss'
+  }
+];
